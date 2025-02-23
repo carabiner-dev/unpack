f305cd8912a226d4cb73d3c6c3d6430f2cf7a61e
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-23 00:09:39 -0600

    Unpacker prototype
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

---
+++ dependencies/unpacker.go
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package dependencies
+
+type Unpacker struct {
+}
+
+func (unpacker *Unpacker) ExtractCodebase()       {}
+func (unpacker *Unpacker) ExtractArtifact(string) {}
