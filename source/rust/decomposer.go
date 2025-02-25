4bf584602fbe899f39d77f34f90137a28b48a826
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-25 17:36:22 -0600

    Commit rust decomposer
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

---
+++ source/rust/decomposer.go
@@ -0,0 +1,171 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package rust
+
+import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/carabiner-dev/unpack/source"
+	"github.com/google/uuid"
+	"github.com/protobom/protobom/pkg/sbom"
+	"sigs.k8s.io/release-utils/command"
+)
+
+var _ source.Decomposer = (*Decomposer)(nil)
+
+func New() *Decomposer {
+	return &Decomposer{}
+}
+
+type Decomposer struct{}
+
+var packageNameRegexString = `^(\d+)(\S+)\s(\S+)`
+var packageNameRegex *regexp.Regexp
+
+// These options not yet wired in
+type Options struct {
+	// These options match the cargo dependency names
+	GenerateNormalDependencies bool
+	GenerateBuildDependencies  bool
+	GenerateDevDependencies    bool
+}
+
+// TODO(puerco): Enrich from https://crates.io/api/v1/crates/quote/1.0.35
+
+func (d *Decomposer) Extract(opts *source.Options) (*sbom.NodeList, error) {
+	var dopts = d.DefaultOptions().(Options)
+	if lo := opts.GetDecomposerOptions(d); lo != nil {
+		var ok bool
+		dopts, ok = lo.(Options)
+		if !ok {
+			return nil, fmt.Errorf("invalid decomposer options type")
+		}
+	}
+	var nl = sbom.NewNodeList()
+	switch {
+	case dopts.GenerateNormalDependencies:
+		if err := d.parseDependencyTree(opts, nl, "normal"); err != nil {
+			return nil, err
+
+		}
+	case dopts.GenerateDevDependencies:
+		if err := d.parseDependencyTree(opts, nl, "dev"); err != nil {
+			return nil, err
+
+		}
+	case dopts.GenerateBuildDependencies:
+		if err := d.parseDependencyTree(opts, nl, "dev"); err != nil {
+			return nil, err
+
+		}
+	}
+	return nl, nil
+}
+
+// parseDependencyTree extracts dependencies from the rust source. This function
+// takes a dependency type string: "normal", "dev", or "build" matching the cargo
+// types.
+func (d *Decomposer) parseDependencyTree(opts *source.Options, nl *sbom.NodeList, depType string) error {
+	var typeParam string
+
+	// Switch these to make sure we dont exec an unknown param:
+	switch depType {
+	case "normal", "dev", "build":
+		typeParam = fmt.Sprintf("--edges=%s", depType)
+	default:
+		return fmt.Errorf("wrong dependency type")
+	}
+
+	// Build the command
+	var cmd *command.Command
+	if opts.WorkDir == "" {
+		cmd = command.New("cargo", "tree", "--prefix=depth", "--no-dedupe", typeParam)
+	} else {
+		cmd = command.NewWithWorkDir(opts.WorkDir, "cargo", "tree", "--prefix=depth", "--no-dedupe", typeParam)
+	}
+
+	// Extract the dependency data
+	output, err := cmd.RunSilentSuccessOutput()
+	if err != nil {
+		return fmt.Errorf("waiting for cargo output: %w", err)
+	}
+
+	seen := map[string]*sbom.Node{}
+
+	if err := d.parseCargoOutput(nl, output.OutputTrimNL(), sbom.Edge_dependsOn, seen); err != nil {
+		return fmt.Errorf("parsing cargo data: %w", err)
+	}
+	return nil
+}
+
+func (d *Decomposer) parseCargoOutput(nl *sbom.NodeList, data string, edgeType sbom.Edge_Type, seen map[string]*sbom.Node) error {
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	if packageNameRegex == nil {
+		packageNameRegex = regexp.MustCompile(packageNameRegexString)
+	}
+
+	var currentLevel = 0
+	var depladder = map[int]*sbom.Node{}
+
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			continue
+		}
+
+		matches := packageNameRegex.FindStringSubmatch(scanner.Text())
+		if matches == nil {
+			return fmt.Errorf("unable to parse cargo output %q %+v", scanner.Text(), matches)
+		}
+
+		version := matches[3]
+		name := matches[2]
+		level, err := strconv.Atoi(matches[1])
+		if err != nil {
+			return fmt.Errorf("error reading depth level")
+		}
+		currentLevel = level
+		purl := fmt.Sprintf("pkg:cargo/%s@%s", name, version)
+
+		// Reuse a node or generate a new one
+		var node *sbom.Node
+		if n, ok := seen[purl]; ok {
+			node = n
+		} else {
+			node = &sbom.Node{
+				Id:          uuid.NewString(),
+				Type:        sbom.Node_PACKAGE,
+				Name:        name,
+				Version:     version,
+				FileName:    name,
+				UrlDownload: fmt.Sprintf("https://crates.io/api/v1/crates/%s/%s/download", name, version),
+				Identifiers: map[int32]string{
+					int32(sbom.SoftwareIdentifierType_PURL): purl,
+				},
+				PrimaryPurpose: []sbom.Purpose{
+					sbom.Purpose_LIBRARY,
+				},
+			}
+			seen[purl] = node
+		}
+		depladder[level] = node
+		if currentLevel > 0 {
+			if err := nl.RelateNodeAtID(node, depladder[level-1].Id, edgeType); err != nil {
+				return fmt.Errorf("linking %q to nodelist: %w", purl, err)
+			}
+		} else {
+			nl.AddRootNode(node)
+		}
+	}
+	return nil
+}
+
+func (d *Decomposer) DefaultOptions() any {
+	return Options{
+		GenerateNormalDependencies: true,
+	}
+}
---
+++ source/rust/decomposer_test.go
@@ -0,0 +1,66 @@
+package rust
+
+import (
+	"testing"
+
+	"github.com/carabiner-dev/unpack/source"
+	"github.com/protobom/protobom/pkg/sbom"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseCargoOutput(t *testing.T) {
+	for _, tc := range []struct {
+		name          string
+		input         string
+		expectedLen   int
+		expectedRoots int
+		mustErr       bool
+	}{
+		{
+			name:          "simple",
+			input:         "0skootrs-bin v0.1.0 (/home/puerco/projects/skootrs/skootrs-bin)\n1base64 v0.22.0\n1clap v4.5.4\n2clap_builder v4.5.2\n3anstream v0.6.13\n",
+			expectedLen:   5,
+			expectedRoots: 1,
+			mustErr:       false,
+		},
+		{
+			name:          "duplicate-dependency",
+			input:         "0skootrs-bin v0.1.0 (/home/puerco/projects/skootrs/skootrs-bin)\n1base64 v0.22.0\n1clap v4.5.4\n2clap_builder v4.5.2\n3anstream v0.6.13\n2anstream v0.6.13\n",
+			expectedLen:   5,
+			expectedRoots: 1,
+			mustErr:       false,
+		},
+		{
+			name:    "bad-input",
+			input:   "skootrs-bin v0.1.0 (/home/puerco/projects/skootrs/skootrs-bin)\n1base64 v0.22.0\n1clap v4.5.4\n2clap_builder v4.5.2\n3anstream v0.6.13\n2anstream v0.6.13\n",
+			mustErr: true,
+		},
+		{
+			name:          "two-roots",
+			input:         "0skootrs-bin v0.1.0 (/home/puerco/projects/skootrs/skootrs-bin)\n1base64 v0.22.0\n1clap v4.5.4\n0clap_builder v4.5.2\n",
+			expectedLen:   4,
+			expectedRoots: 2,
+			mustErr:       false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			nl := sbom.NewNodeList()
+
+			d := New()
+			opts := source.Options{}
+			opts.SetDecomposerOptions(d, Options{
+				GenerateNormalDependencies: true,
+			})
+
+			err := d.parseCargoOutput(nl, tc.input, sbom.Edge_dependsOn, map[string]*sbom.Node{})
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Len(t, nl.Nodes, tc.expectedLen)
+			require.Len(t, nl.RootElements, tc.expectedRoots)
+		})
+	}
+}
