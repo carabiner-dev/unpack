a1a8f5015cd301f879274384fe84131c73294ffa
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-25 15:43:20 -0600

    Fix panic in unset dec opts
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- source/options.go
+++ source/options.go
@@ -11,10 +11,16 @@ func (so *Options) SetDecomposerOptions(dec Decomposer, opts any) {
 	if dec == nil {
 		return
 	}
+	if so.decomposerOptions == nil {
+		so.decomposerOptions = map[string]any{}
+	}
 	so.decomposerOptions[fmt.Sprintf("%T", dec)] = opts
 }
 
 func (so *Options) GetDecomposerOptions(dec Decomposer) any {
+	if so.decomposerOptions == nil {
+		return nil
+	}
 	if opts, ok := so.decomposerOptions[fmt.Sprintf("%T", dec)]; ok {
 		return opts
 	}
