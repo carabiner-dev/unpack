112bc5b48ca1a5f86b5548e1b9bdda6e22ea876b
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-02-25 21:05:16 -0600

    Add code pkg & indexer
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ code/index.go
@@ -0,0 +1,39 @@
+package code
+
+import (
+	"io/fs"
+	"path/filepath"
+)
+
+type PathIndex map[string][]string
+
+// Adds an entry to the filesystem
+func (pi PathIndex) Add(dirname, filename string) {
+	if _, ok := pi[dirname]; !ok {
+		pi[dirname] = []string{}
+	}
+	pi[dirname] = append(pi[dirname], filename)
+}
+
+type Indexer struct {
+}
+
+// CatalogFiles traverses a filesystem and returns an index of all directories
+// with their files
+func (i *Indexer) CatalogDirectories(path string) (*PathIndex, error) {
+	index := PathIndex{}
+	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || !d.Type().IsRegular() {
+			return nil
+		}
+		index.Add(filepath.Dir(path), d.Name())
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &index, nil
+}
